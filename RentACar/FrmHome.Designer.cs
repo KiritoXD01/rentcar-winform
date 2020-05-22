eaa6db1622a3b4bb523bf02d23fd39d982d5a97d
Javier Emmanuel Mercedes <manuelmercedez10@gmail.com>
2020-05-22 01:11:47 -0400

    Agregar archivos de proyecto.

---
+++ RentACar/FrmHome.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace RentACar
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHome));
+            this.Sidebar = new System.Windows.Forms.Panel();
+            this.panelVehiculo = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnVehiculoPanel = new System.Windows.Forms.Button();
+            this.SidebarLogo = new System.Windows.Forms.Panel();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Sidebar.SuspendLayout();
+            this.panelVehiculo.SuspendLayout();
+            this.panelContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Sidebar
+            // 
+            this.Sidebar.AutoScroll = true;
+            this.Sidebar.BackColor = System.Drawing.Color.Black;
+            this.Sidebar.Controls.Add(this.panelVehiculo);
+            this.Sidebar.Controls.Add(this.btnVehiculoPanel);
+            this.Sidebar.Controls.Add(this.SidebarLogo);
+            this.Sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Sidebar.Location = new System.Drawing.Point(0, 0);
+            this.Sidebar.Name = "Sidebar";
+            this.Sidebar.Size = new System.Drawing.Size(250, 561);
+            this.Sidebar.TabIndex = 0;
+            // 
+            // panelVehiculo
+            // 
+            this.panelVehiculo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panelVehiculo.Controls.Add(this.button5);
+            this.panelVehiculo.Controls.Add(this.button4);
+            this.panelVehiculo.Controls.Add(this.button3);
+            this.panelVehiculo.Controls.Add(this.button2);
+            this.panelVehiculo.Controls.Add(this.button1);
+            this.panelVehiculo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelVehiculo.Location = new System.Drawing.Point(0, 147);
+            this.panelVehiculo.Name = "panelVehiculo";
+            this.panelVehiculo.Size = new System.Drawing.Size(250, 207);
+            this.panelVehiculo.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button5.Location = new System.Drawing.Point(0, 160);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(250, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Vehiculos";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button4.Location = new System.Drawing.Point(0, 120);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(250, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Combustibles";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(0, 80);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(250, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Modelos";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(0, 40);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(250, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Marcas";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(250, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tipo de vehiculo";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnVehiculoPanel
+            // 
+            this.btnVehiculoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnVehiculoPanel.FlatAppearance.BorderSize = 0;
+            this.btnVehiculoPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVehiculoPanel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnVehiculoPanel.Location = new System.Drawing.Point(0, 102);
+            this.btnVehiculoPanel.Name = "btnVehiculoPanel";
+            this.btnVehiculoPanel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnVehiculoPanel.Size = new System.Drawing.Size(250, 45);
+            this.btnVehiculoPanel.TabIndex = 0;
+            this.btnVehiculoPanel.Text = "Vehiculos";
+            this.btnVehiculoPanel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVehiculoPanel.UseVisualStyleBackColor = true;
+            this.btnVehiculoPanel.Click += new System.EventHandler(this.btnVehiculoPanel_Click);
+            // 
+            // SidebarLogo
+            // 
+            this.SidebarLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SidebarLogo.Location = new System.Drawing.Point(0, 0);
+            this.SidebarLogo.Name = "SidebarLogo";
+            this.SidebarLogo.Size = new System.Drawing.Size(250, 102);
+            this.SidebarLogo.TabIndex = 0;
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.Gray;
+            this.panelContainer.Controls.Add(this.pictureBox1);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(250, 0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(684, 561);
+            this.panelContainer.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(684, 561);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.Sidebar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "FrmHome";
+            this.Text = "Rent a Car";
+            this.Sidebar.ResumeLayout(false);
+            this.panelVehiculo.ResumeLayout(false);
+            this.panelContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Sidebar;
+        private System.Windows.Forms.Button btnVehiculoPanel;
+        private System.Windows.Forms.Panel SidebarLogo;
+        private System.Windows.Forms.Panel panelVehiculo;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RentACar/FrmHome.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentACar
+{
+    public partial class FrmHome : Form
+    {
+        private Form activeForm = null;
+
+        public FrmHome()
+        {
+            InitializeComponent();
+            CustomizeDesign();
+        }
+
+        private void CustomizeDesign()
+        {
+            panelVehiculo.Visible = false;
+        }
+
+        private void HideSubMenu()
+        {
+            if (panelVehiculo.Visible == true)
+            {
+                panelVehiculo.Visible = false;
+            }
+        }
+
+        private void ShowSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                HideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        private void btnVehiculoPanel_Click(object sender, EventArgs e)
+        {
+            ShowSubMenu(panelVehiculo);
+        }
+
+
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            panelContainer.Controls.Add(childForm);
+            panelContainer.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openChildForm(new FrmTipoVehiculo());
+        }
+    }
+}
